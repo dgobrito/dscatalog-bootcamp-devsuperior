07006f9f42d0016a200e6e1bc5f93a8b0ebcaa76
Dyego dos Santos Brito <dyego.eti@gmail.com>
2020-10-05 06:50:33 -0300

    H2 Database

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dspesquisa.entities.Category;
+import com.devsuperior.dspesquisa.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();		
+	}
+}
