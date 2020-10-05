bba461ff22f2f38d43bbd19e5417a3344df7a414
Dyego dos Santos Brito <dyego.eti@gmail.com>
2020-10-05 07:31:20 -0300

    CategoryDTO

--- backend/src/main/java/com/devsuperior/dspesquisa/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dspesquisa.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dspesquisa.dto.CategoryDTO;
 import com.devsuperior.dspesquisa.entities.Category;
 import com.devsuperior.dspesquisa.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();		
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList()); 		
 	}
 }
