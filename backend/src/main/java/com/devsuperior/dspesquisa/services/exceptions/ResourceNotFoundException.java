9095f1256525a1a7ddcba651485eefb8de2c0001
Dyego dos Santos Brito <dyego.eti@gmail.com>
2020-10-06 07:25:41 -0300

    Update Category

--- backend/src/main/java/com/devsuperior/dspesquisa/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/exceptions/ResourceNotFoundException.java
@@ -2,11 +2,11 @@ package com.devsuperior.dspesquisa.services.exceptions;
 
 // Exception = Obrigatoriamente precisa ser tratada, o compilador irá obrigar
 // RuntimeException = Mais Flexível, posso ou não tratar
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public EntityNotFoundException(String msg) {
+	public ResourceNotFoundException(String msg) {
 		super(msg);		
 	}
 }
