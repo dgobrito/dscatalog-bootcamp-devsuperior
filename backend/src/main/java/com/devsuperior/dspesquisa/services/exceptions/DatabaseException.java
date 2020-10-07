82ca25e777aa3b6be572d80fa1ee05d2bc201a46
Dyego dos Santos Brito <dyego.eti@gmail.com>
2020-10-07 07:34:49 -0300

    Delete Category

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dspesquisa.services.exceptions;
+
+// Exception = Obrigatoriamente precisa ser tratada, o compilador irá obrigar
+// RuntimeException = Mais Flexível, posso ou não tratar
+public class DatabaseException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);		
+	}
+}
