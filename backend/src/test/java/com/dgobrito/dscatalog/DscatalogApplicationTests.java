938bb8b4122c693d4f3598e1d18665c8a76f96ad
Dyego dos Santos Brito <dyego.eti@gmail.com>
2022-07-06 19:14:11 -0300

    Backend project created

---
+++ backend/src/test/java/com/dgobrito/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgobrito.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
