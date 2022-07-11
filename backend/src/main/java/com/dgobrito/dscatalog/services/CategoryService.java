f04ba9b7027a64d33efbbb44d4eaaf7700acc1d8
Dyego dos Santos Brito <dyego.eti@gmail.com>
2022-07-11 20:04:32 -0300

    DTO

--- backend/src/main/java/com/dgobrito/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dgobrito/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.dgobrito.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dgobrito.dscatalog.dto.CategoryDTO;
 import com.dgobrito.dscatalog.entities.Category;
 import com.dgobrito.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +16,22 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();		
+	/*
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		List<CategoryDTO> listDto = new ArrayList<>();
+		
+		for (Category entity : list) {
+			listDto.add(new CategoryDTO(entity));			
+		}
+		
+		return listDto;
 	}
+	*/
+	
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}	
 }
