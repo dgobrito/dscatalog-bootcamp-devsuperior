b50d1972f723ad151eeb51886c543b5fae3d65b4
Dyego dos Santos Brito <dyego.eti@gmail.com>
2022-08-22 19:58:56 -0300

    Delete category

--- backend/src/main/java/com/dgobrito/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dgobrito/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dgobrito.dscatalog.dto.CategoryDTO;
 import com.dgobrito.dscatalog.entities.Category;
 import com.dgobrito.dscatalog.repositories.CategoryRepository;
+import com.dgobrito.dscatalog.services.exceptions.DatabaseException;
 import com.dgobrito.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -66,6 +69,16 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);			
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);						
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");						
 		}
 	}	
 }
