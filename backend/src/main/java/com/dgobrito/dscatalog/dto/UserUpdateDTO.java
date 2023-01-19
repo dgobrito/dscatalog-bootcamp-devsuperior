e62d27167b40c66e740fbd08680103e922c79638
Dyego dos Santos Brito <dyego.eti@gmail.com>
2023-01-19 19:22:31 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/dgobrito/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.dgobrito.dscatalog.dto;
+
+import com.dgobrito.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	
+	private static final long serialVersionUID = 1L;
+}
--- backend/src/main/java/com/dgobrito/dscatalog/services/UserService.java
+++ backend/src/main/java/com/dgobrito/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dgobrito.dscatalog.dto.RoleDTO;
 import com.dgobrito.dscatalog.dto.UserDTO;
 import com.dgobrito.dscatalog.dto.UserInsertDTO;
+import com.dgobrito.dscatalog.dto.UserUpdateDTO;
 import com.dgobrito.dscatalog.entities.Role;
 import com.dgobrito.dscatalog.entities.User;
 import com.dgobrito.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToEntity(dto, entity);
